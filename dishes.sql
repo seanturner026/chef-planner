f1a5a56f754cdc7b6eb04b05a19df07ff4307cbd
SeanTurner026 <sean.turner026@gmail.com>
2018-03-01 18:03:32 +0900

    added dishes.sql which contains sql code that builds the postgres database

---
+++ dishes.sql
@@ -0,0 +1,21 @@
+-- Table: public.dishes
+
+-- DROP TABLE public.dishes;
+
+CREATE TABLE public.dishes
+(
+    id integer NOT NULL DEFAULT nextval('dishes_id_seq'::regclass),
+    dish text COLLATE pg_catalog."default",
+    total_duration integer,
+    duration integer[],
+    instructions text[] COLLATE pg_catalog."default",
+    description text COLLATE pg_catalog."default",
+    CONSTRAINT dishes_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+TABLESPACE pg_default;
+
+ALTER TABLE public.dishes
+    OWNER to sean;
